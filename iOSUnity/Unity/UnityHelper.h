1bc8c5da2d6f0029ad2a973c69ab1ce540fce7f6
LiqunWu <lqwu@nreal.ai>
2023-03-02 15:57:01 +0800

    demo

---
+++ iOSUnity/Unity/UnityHelper.h
@@ -0,0 +1,16 @@
+//
+//  UnityHelper.h
+//  iOSUnity
+//
+//  Created by Nreal on 2023/3/2.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UnityHelper : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSUnity/Unity/UnityHelper.m
@@ -0,0 +1,12 @@
+//
+//  UnityHelper.m
+//  iOSUnity
+//
+//  Created by Nreal on 2023/3/2.
+//
+
+#import "UnityHelper.h"
+
+@implementation UnityHelper
+
+@end
